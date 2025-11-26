e7f68ac2390d284f7e72504ec272b8629add6a21
Hugo <huwg@mailbox.org>
2025-11-26 17:51:46 +0100

    try

--- text.ts
+++ text.ts
@@ -342,7 +342,7 @@ namespace helpers {
     }
 
     //% blockId=imageShowDataView
-    //% block="show data view in $img=theScreen | headline $headline || $label1 $value1 | $label2 $value2 | $label3 $value3 || color $color=colorindexpicker font $font"
+    //% block="show data view in $img=theScreen headline $headline || $label1 $value1 || $label2 $value2 || $label3 $value3 || color $color=colorindexpicker font $font"
     //% blockNamespace="drawing" group="Text"
     //% weight=85
     //% expandableArgumentMode="toggle"
